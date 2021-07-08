297d19f35a26bf707abdad6334c7a0b7d963f8fa
Steven Smith <github@icodeforyou.com>
2021-07-08 12:19:57 -0400

    added dropbox content to ios appdelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import ObjectiveDropboxOfficial
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,15 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if let authResult = DBClientsManager.handleRedirectURL(url) {
+              if authResult.isSuccess() {
+                  print("dropbox auth success")
+              } else if (authResult.isCancel()) {
+                  print("dropbox auth cancel")
+              } else if (authResult.isError()) {
+                  print("dropbox auth error \(authResult.errorDescription)")
+              }
+          }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/modular/scaffold_widget.dart
+++ lib/modular/scaffold_widget.dart
@@ -1,3 +1,4 @@
+import 'package:dropbox_client/dropbox_client.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_extras/flutter_extras.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -5,6 +6,10 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:theme_manager/theme_manager.dart';
 
 import '../goal_display/goal_module.dart';
+
+const String _dropbox_clientId = 'test-flutter-dropbox';
+const String _dropbox_key = 'jdngwadvorh5zue';
+const String _dropbox_secret = 'jdngwadvorh5zue';
 
 class ScaffoldWidget extends StatefulWidget {
   ScaffoldWidget({Key? key, required this.title}) : super(key: key);
@@ -15,6 +20,12 @@ class ScaffoldWidget extends StatefulWidget {
 }
 
 class _ScaffoldWidget extends ObservingStatefulWidget<ScaffoldWidget> {
+  @override
+  initState() {
+    super.initState();
+    _initDropbox();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,5 +56,10 @@ class _ScaffoldWidget extends ObservingStatefulWidget<ScaffoldWidget> {
         ],
       ),
     );
+  }
+
+  Future _initDropbox() async {
+    bool flag = await Dropbox.init(_dropbox_clientId, _dropbox_key, _dropbox_secret);
+    debugPrint('FLAG $flag');
   }
 }
